3372c20260d224aa16f27af3192c80ccfa71c7d1
Josemaria <josemarianriagu25@gmail.com>
2019-10-23 23:57:41 +0800

    return user id in login resonse

--- controllers/index.js
+++ controllers/index.js
@@ -40,6 +40,7 @@ const loginUser = async (req, res, next) => {
     return res.status(200).json({
       message: `Welcome ${username}`,
       token,
+      id: user.id,
     });
   } catch (error) {
     next(error);
